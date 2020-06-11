455fce3cc6f20d7fe34c78beb168a6d00edcc6b9
swanmatch <swan_match@yahoo.co.jp>
2020-06-12 03:04:19 +0900

    adjust left sum key

--- keyboards/colosseum44/rev1/keymaps/swan/keymap.c
+++ keyboards/colosseum44/rev1/keymaps/swan/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q, KC_W, KC_E, KC_R, KC_T,                KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_EQL,
     KC_LSFT,   KC_A, KC_S, KC_D, KC_F, KC_G,                KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LCTRL,  KC_Z, KC_X, KC_C, KC_V, KC_B,                KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-     LALT_T(KC_SPC), KC_BSPC, CALC, LSFT_T(KC_F5),    GUI_T(KC_F12), CUSL, KC_DEL, LCTL_T(KC_ENT)
+     LALT_T(KC_F5), KC_BSPC, LSFT_T(KC_SPC), CALC,    GUI_T(KC_F12), CUSL, KC_DEL, LCTL_T(KC_ENT)
   ),
 
 /* Cursol
